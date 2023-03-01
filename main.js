f89dbc2ef3d6fe3c90384acf9add46c93709078a
Daniel Sin <danielesin@gmail.com>
2023-03-01 16:17:05 -0500

    Youtube feed is removed. Can't get the picture to show. Reddit feed not working

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 let contents = document.getElementById('contents');
 // let primaryParent = primary.parent();
 let contentsParent = contents.parentNode;
+let container = document.getElementsByClassName('rpBJOHq2PR60pnwJlUyP0');
 
-let img = document.createElement("img");
+// let img = document.createElement("img");
+let img = new Image();
 let div = document.createElement("div");
 
 
 div.innerText = "Focus!";
-img.src = "no_tv.png";
+img.src = "./no_tv.png";
 
 contentsParent.prepend(div);
 contentsParent.prepend(img);
 contents.parentNode.removeChild(contents);
+container.parentNode.removeChild(container);
