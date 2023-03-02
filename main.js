f6d28653cce9845b45aa002984c0dcda0cb8b4e5
Daniel Sin <danielesin@gmail.com>
2023-03-02 14:39:41 -0500

    image working on youtube.

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
+// youtube
+
 let contents = document.getElementById('contents');
-// let primaryParent = primary.parent();
 let contentsParent = contents.parentNode;
-let container = document.getElementsByClassName('rpBJOHq2PR60pnwJlUyP0');
 
 // let img = document.createElement("img");
 let img = new Image();
 let div = document.createElement("div");
 
+div.classList.add('beautText');
 
 div.innerText = "Focus!";
-img.src = "./no_tv.png";
+img.src = "https://cdn-icons-png.flaticon.com/512/482/482862.png";
 
 contentsParent.prepend(div);
 contentsParent.prepend(img);
 contents.parentNode.removeChild(contents);
-container.parentNode.removeChild(container);
+
+
+
+// reddit
+
+// works in console but not as extension
+var twoX = document.getElementById('2x-container');
+var twoXParent = twoX.parentNode;
+// twoXParent.prepend(img);
+// twoX.parentNode.removeChild(twoX);
+
+// let container = document.getElementsByClassName('rpBJOHq2PR60pnwJlUyP0');
+// container.parentNode.removeChild(container);
